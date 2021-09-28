9ad19469d78f6a7ee9cf37bb811bdad61aa7c407
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-28 15:39:39 +0200

    Added solution of the second query

--- query_1.sql
+++ query_1.sql
@@ -1,5 +1,11 @@
 -- 1) Selezionare tutti gli studenti nati nel 1990: 
-
 SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990;
+
+
+-- 2) Selezionare tutti i corsi che valgono più di 10 crediti: 
+SELECT `name`, `cfu`
+FROM `courses`
+WHERE `cfu` > 10; 
+
