806197d72d901326f648778e8012d00059cff5c0
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-28 16:32:26 +0200

    Added solution of the second query with "group by"

--- query_2.sql
+++ query_2.sql
@@ -1,4 +1,9 @@
--- 1) Contare quanti iscritti ci sono stati ogni anno 
+-- 1) Contare quanti iscritti ci sono stati ogni anno: 
 SELECT YEAR(`enrolment_date`) AS `year`, COUNT(*) AS `tot_enrolled`
 FROM `students`
 GROUP BY `year`;
+
+-- 2) Contare gli insegnanti che hanno l'ufficio nello stesso edificio: 
+SELECT `office_address`, COUNT(*) AS `tot_teachers`
+FROM `teachers`
+GROUP BY `office_address`;
