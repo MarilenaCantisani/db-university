9acd19b7e38c7ef46be506ac831162e2782e9984
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-29 15:25:39 +0200

    Added third query

--- query_3.sql
+++ query_3.sql
@@ -10,4 +10,13 @@ SELECT `degrees`.`name`
 FROM `degrees`
 JOIN `departments`
 ON `departments`.`id` = `degrees`.`department_id`
-WHERE `departments`.`name` = 'Dipartimento di Neuroscienze'
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3) Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `courses`.`name`
+FROM `course_teacher`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44;
